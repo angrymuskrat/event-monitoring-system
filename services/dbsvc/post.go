e4fa95ce8a352e162225f224fed97b37456c831a
Mikhail <mlhakov2011@gmail.com>
2019-10-19 15:33:27 +0300

    fix folder name

--- services/dbsvc/post.go
+++ services/dbsvc/post.go
@@ -3,35 +3,13 @@ package dbsvc
 import (
 	"context"
 	"fmt"
+
+	"github.com/angrymuskrat/event-monitoring-system/services/dbsvc/pb"
 )
 
-type Post struct {
-	ID            string
-	Shortcode     string
-	ImageURL      string
-	IsVideo       bool
-	Caption       string
-	CommentsCount int
-	Timestamp     int64
-	LikesCount    int
-	IsAd          bool
-	AuthorID      string
-	LocationID    string
-	Lat           float64
-	Lon           float64
-}
-
-type SpatialTemporalInterval struct {
-	MinTime int64
-	MaxTime int64
-	MinLat  float64
-	MinLon  float64
-	MaxLat  float64
-	MaxLon  float64
-}
 
 type Repository interface {
-	PushPosts(ctx context.Context, posts []Post) error
+	PushPosts(ctx context.Context, posts []pd.Post) error
 	SelectPosts(ctx context.Context, interval SpatialTemporalInterval) ([]Post, error)
 }
 
