02915a3dd3f7e72c59128a97eefe8ba366a80762
visheratin <alexvish91@gmail.com>
2020-02-02 09:23:38 +0300

    Modified data generator.

--- services/data-storage/cmd/client/testGen.go
+++ services/data-storage/cmd/client/testGen.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	data "github.com/angrymuskrat/event-monitoring-system/services/proto"
 	"math/rand"
 	"time"
+
+	data "github.com/angrymuskrat/event-monitoring-system/services/proto"
 )
 
 var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -21,7 +22,7 @@ func RandString(length int) string {
 }
 
 func RandBool() bool {
-	return seededRand.Int() % 2 == 1
+	return seededRand.Int()%2 == 1
 }
 
 func RandInt64(maxV int64) int64 {
@@ -29,27 +30,27 @@ func RandInt64(maxV int64) int64 {
 }
 
 func RandDouble() float64 {
-	return float64(seededRand.Int() % (maxLoc - 1)) + seededRand.Float64();
+	return float64(seededRand.Int()%(maxLoc-1)) + seededRand.Float64()
 }
 
 func GeneratePosts(n int) []data.Post {
-	posts := *new([]data.Post)
+	posts := make([]data.Post, n)
 	for i := 0; i < n; i++ {
-		posts = append(posts, data.Post{ ID: RandString(20), Shortcode: RandString(10), ImageURL: RandString(30),
+		posts[i] = data.Post{ID: RandString(20), Shortcode: RandString(10), ImageURL: RandString(30),
 			IsVideo: RandBool(), Caption: RandString(100), CommentsCount: RandInt64(1000), Timestamp: RandInt64(1000),
 			LikesCount: RandInt64(1000), IsAd: RandBool(), AuthorID: RandString(15), LocationID: RandString(15),
-			Lat: RandDouble(), Lon: RandDouble()})
+			Lat: RandDouble(), Lon: RandDouble()}
 	}
-	return posts;
+	return posts
 }
 
 func GeneratePostsFast(n int) []data.Post {
-	posts := *new([]data.Post)
+	posts := make([]data.Post, n)
 	for i := 0; i < n; i++ {
-		posts = append(posts, data.Post{ ID: RandString(20), Shortcode: "sfsfhhsgfhsf", ImageURL: "RandString(30)",
+		posts[i] = data.Post{ID: RandString(20), Shortcode: "sfsfhhsgfhsf", ImageURL: "RandString(30)",
 			IsVideo: false, Caption: "RandString(100)", CommentsCount: 1000, Timestamp: 1000,
 			LikesCount: 1000, IsAd: false, AuthorID: "RandString(15)", LocationID: "RandString(15)",
-			Lat: 0, Lon: 0 })
+			Lat: 0, Lon: 0}
 	}
-	return posts;
+	return posts
 }
