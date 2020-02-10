15ed1d0e1b92d2051817e7def2756304e64f25ee
Mikhail <mlhakov2011@gmail.com>
2020-02-10 13:10:13 +0300

    add services methods description

--- services/data-storage/service.go
+++ services/data-storage/service.go
@@ -8,49 +8,95 @@ import (
 )
 
 const (
-	TimeWaitingClient = 30 * time.Second // in seconds
-	MaxMsgSize        = 1000000000       // in bytes
+	// Max time of waiting of execution of request for client; time.Duration
+	TimeWaitingClient = 30 * time.Second
+
+	// Max size for income messages of grpcService; in bytes
+	// For client is needed also set max income message side, but this is done by the client during initialization of grpcClient
+	MaxMsgSize        = 1000000000
 )
 
 type Service interface {
+	// input: context, a city object
+	// output: error
+	// result: if the city was successfully added, will return nil, otherwise will return an error
 	InsertCity(ctx context.Context, city data.City, updateIfExists bool) error
 
+	// input: context
+	// output: array of city objects, error
+	// result: if request was successfully finished, will return cities and nil error, otherwise will return empty array and error
 	GetAllCities(ctx context.Context) ([]data.City, error)
 
+	// input: context, city.Code - id of the city
+	// output: city and error
+	// result: if request was successfully finished, will return the city and nil error, otherwise return nil and some error
 	GetCity(ctx context.Context, cityId string) (*data.City, error)
 
-	// input array of Posts and write every Post to database
-	// return array of statuses of adding posts
-	// 		and error if one or more Post wasn't pushed
-	PushPosts(ctx context.Context, cityId string, posts []data.Post) ([]int32, error)
+	// input: context, cityId string, array of posts
+	// output: array of commit statuses (see data-storage/data/typePushResponse) and error
+	// if all posts were successfully added to the city's db, will return statuses and nil error, otherwise statuses and some error
+	// Either all posts will be added or not a single one.
+	PushPosts(ctx context.Context, cityId string, posts []data.Post) ([]int32, error) // TODO remove []int32 from data-storage and insta-crawler
 
-	// input SpatioTemporalInterval
-	// return array of post, every of which satisfy the interval conditions
-	// 		and error if storage can't return posts due to other reasons
+	// input: context, id of the city, start and finish UTC-time in second - time interval, for which posts of this city will be returned
+	// output: array of posts, area object, error
+	// result: if request was successfully finished, will return posts and Border of city - TopLeft and BotRight Points and nil error,
+	// 		otherwise, empty array, nil area and some error
 	SelectPosts(ctx context.Context, cityId string, startTime, finishTime int64) ([]data.Post, *data.Area, error)
 
+	// input: context, id of the city, interval, which contains UTC-time in second - start time of needed hour and
+	// 		area - TopLeft and BotRight Points of needed space
+	// output: array of aggregated posts, each aggr post has coordinate of its aggregated cell, and amount of posts in this hour and this cell
+	// result: if request was successfully finished will return aggregated posts and nil error, otherwise empty array and some error
 	SelectAggrPosts(ctx context.Context, cityId string, interval data.SpatioHourInterval) ([]data.AggregatedPost, error)
 
+	// input: context, id of the city, start hour and finish hour UTC-time in seconds, both are beginning of needed hours
+	// output: array of timestamps and error
+	// result: if request was successfully finished, will return timeline - amount of posts and events in this city between start and finish hour
 	PullTimeline(ctx context.Context, cityId string, start, finish int64) ([]data.Timestamp, error)
 
-	// input not empty id and not empty array of bytes
-	// if blob successfully added to database, return nil
-	// else return error
+	// input: context, id of the city, map of grids, keys of this map are ids and value is byte array - historic grid.
+	// 		ids description: first two digit: month; 3th: 0 - work day,  1 - holiday; 4th and 5th: hour.
+	//		Example: 02013 - 02 March, 0 work day, 13 o'clock
+	// output: error
+	// if all grids were successfully added to the city's db, will return nil error, otherwise statuses and some error
+	// Either all grids will be added or not a single one.
 	PushGrid(ctx context.Context, cityId string, grids map[int64][]byte) error
 
-	// input not empty id
-	// if there are exist some blob with the id in database return the blob
-	// else return error
+	// input: context, id of the city, start and finish ids
+	// 		ids description: first two digit: month; 3th: 0 - work day,  1 - holiday; 4th and 5th: hour.
+	//		Example: 02013 - 02 March, 0 work day, 13 o'clock
+	// output: map of map of grids, keys of this map are ids and value is byte array
+	// result: if request was successfully finished, will return grids and nil error otherwise return nil map and some error
 	PullGrid(ctx context.Context, cityId string, startId, finishId int64) (map[int64][]byte, error)
 
+	// input: context, id of the city, array od events
+	// output: error
+	// if all events were successfully added to the city's db, will return nil error, otherwise statuses and some error
+	// Either all events will be added or not a single one.
 	PushEvents(ctx context.Context, cityId string, events []data.Event) error
 
+	// input: context, id of the city, interval, which contains UTC-time in second - start time of needed hour and
+	// 		area - TopLeft and BotRight Points of needed space
+	// output: events and error
+	// result: if request was successfully finished, will return events and nil error, otherwise, empty array and some error
 	PullEvents(ctx context.Context, cityId string, interval data.SpatioHourInterval) ([]data.Event, error)
 
+	// input: context, id of the city, array of tags, start hour and finish hour UTC-time in seconds, both are beginning of needed hours
+	// output: events and error
+	// result: if request was successfully finished, will return events,
+	//		which have tags from request and time between startTime and finishTime and nil error, otherwise, empty array and some error
 	PullEventsTags(ctx context.Context, cityId string, tags []string, startTime, finishTime int64) ([]data.Event, error)
 
+	// input: context, id of the city, array of locations - Instagram's locations for this city
+	// output: error
+	// if all locations were successfully added to the city's db, will return nil error, otherwise statuses and some error
+	// Either all locations will be added or not a single one.
 	PushLocations(ctx context.Context, cityId string, locations []data.Location) error
 
+	// input: context, id of the city
+	// output: array of locations
+	// result: if request was successfully finished, return all locations of this city and nil error, otherwise return some error
 	PullLocations(ctx context.Context, cityId string) ([]data.Location, error)
 }
 
