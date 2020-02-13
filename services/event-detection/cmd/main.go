99b7ec11ddac62f1d61c7879db361f85cd83f9e3
UfaSemen <ufasemen.93@gmail.com>
2020-02-13 15:00:44 +0300

    start event detection fix

--- services/event-detection/cmd/main.go
+++ services/event-detection/cmd/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"context"
 	"flag"
 
-	"github.com/BurntSushi/toml"
-	service "github.com/angrymuskrat/event-monitoring-system/services/data-storage"
+	service "github.com/angrymuskrat/event-monitoring-system/services/event-detection/service"
 	"github.com/visheratin/unilog"
-	"go.uber.org/zap"
 )
 
 func main() {
@@ -19,12 +18,5 @@ func main() {
 	logCfg.ErrorOutputPaths = append(logCfg.ErrorOutputPaths, *lp)
 	unilog.InitLog(logCfg)
 
-	var cfg service.Config
-	_, err := toml.DecodeFile(*cp, &cfg)
-	if err != nil {
-		unilog.Logger().Error("unable to read config file", zap.String("path", *cp), zap.Error(err))
-		panic(err)
-	}
-
-	service.ServerStart(cfg)
+	service.Start(context.Background(), *cp)
 }
