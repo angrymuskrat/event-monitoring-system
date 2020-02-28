ac8ed35c283a5210eaec237378fa7742e7a9394a
visheratin <alexvish91@gmail.com>
2020-02-28 05:26:34 +0300

    Changed event detection logic.

--- services/event-detection/detection/events.go
+++ services/event-detection/detection/events.go
@@ -38,151 +38,10 @@ func treeEvents(tree *convtree.ConvTree, maxPoints int, filterTags map[string]bo
 	if tree.IsLeaf {
 		result := []data.Event{}
 		if len(tree.Points) >= maxPoints {
-			//tagPosts := map[string]map[string]bool{}
-			posts := make([]data.Post, len(tree.Points))
-			evHolders := []eventHolder{}
-			for i, item := range tree.Points {
-				tags := []string{}
-				post := item.Content.(data.Post)
-				posts[i] = post
-				//tags := []string{}
-				r, _ := regexp.Compile("#[a-zA-Z0-9а-яА-Я_]+")
-				hashtags := r.FindAllString(post.Caption, -1)
-				for idx := range hashtags {
-					hashtags[idx] = strings.ToLower(hashtags[idx])
-				}
-				for _, tag := range hashtags {
-					tag := strings.ToLower(tag)
-					if filterTag(tag, filterTags) {
-						continue
-					}
-					tags = append(tags, tag)
-				}
-				r, _ = regexp.Compile("@[[a-zA-Z0-9а-яА-Я_]+")
-				usernames := r.FindAllString(post.Caption, -1)
-				for idx := range usernames {
-					usernames[idx] = strings.ToLower(usernames[idx])
-				}
-				for _, tag := range usernames {
-					if filterTag(tag, filterTags) {
-						continue
-					}
-					tags = append(tags, tag)
-				}
-				if len(tags) == 0 {
-					continue
-				}
-				exists := false
-				for _, tag := range tags {
-					for i, h := range evHolders {
-						if _, ok := h.tags[tag]; ok {
-							h.users[post.AuthorID] = true
-							h.posts[post.Shortcode] = true
-							for _, t := range tags {
-								if _, ok := h.tags[t]; ok {
-									h.tags[t]++
-								} else {
-									h.tags[t] = 1
-								}
-							}
-							evHolders[i] = h
-							exists = true
-							break
-						}
-					}
-					if exists {
-						break
-					}
-				}
-				if !exists {
-					h := eventHolder{
-						users: map[string]bool{},
-						posts: map[string]bool{},
-						tags:  map[string]int{},
-					}
-					h.users[post.AuthorID] = true
-					h.posts[post.Shortcode] = true
-					for _, tag := range tags {
-						h.tags[tag] = 1
-					}
-					evHolders = append(evHolders, h)
-				}
-				//if len(tags) > 0 {
-				//for _, tag := range tags {
-				//	if _, ok := tagPosts[tag]; !ok {
-				//		tagPosts[tag] = map[string]bool{}
-				//	}
-				//	tagPosts[tag][post.Shortcode] = true
-				//}
-				//}
-			}
-			//for tag, tPosts := range tagPosts {
-			//	added := false
-			//	for tCode := range tPosts {
-			//		for idx, holder := range evHolders {
-			//			if _, ok := holder.posts[tCode]; ok {
-			//				added = true
-			//				for eCode := range tPosts {
-			//					holder.posts[eCode] = true
-			//				}
-			//				holder.tags[tag]++
-			//				evHolders[idx] = holder
-			//			}
-			//		}
-			//	}
-			//	if !added {
-			//		holder := eventHolder{
-			//			posts: map[string]bool{},
-			//			tags:  map[string]int{},
-			//		}
-			//		for code := range tPosts {
-			//			holder.posts[code] = true
-			//		}
-			//		holder.tags[tag] = 1
-			//		evHolders = append(evHolders, holder)
-			//	}
-			//}
+			evHolders, posts := eventHolders(tree.Points, filterTags)
 			for _, e := range evHolders {
-				if len(e.users) < 2 {
-					continue
-				}
-				if len(e.posts) < maxPoints/2 {
-					continue
-				}
-				for k, v := range e.tags {
-					if v < len(e.posts)/3 {
-						delete(e.tags, k)
-					}
-				}
-				//eventPosts := map[string]bool{}
-				//for tag := range e.tags {
-				//	for p := range tagPosts[tag] {
-				//		eventPosts[p] = true
-				//	}
-				//}
-				postCodes := []string{}
-				for k := range e.posts {
-					postCodes = append(postCodes, k)
-				}
-				tags := []string{}
-				var max int
-				var maxTag string
-				for k := range e.tags {
-					if e.tags[k] > max {
-						max = e.tags[k]
-						maxTag = k
-					}
-					tags = append(tags, k)
-				}
-				event := data.Event{
-					Center:    eventCenter(e.posts, posts),
-					PostCodes: postCodes,
-					Tags:      tags,
-					Title:     maxTag,
-					Start:     start,
-					Finish:    finish,
-				}
-				if len(postCodes) > 0 {
+				event, ok := checkEvent(e, maxPoints, posts, start, finish)
+				if ok {
 					result = append(result, event)
 				}
 
@@ -197,6 +56,165 @@ func treeEvents(tree *convtree.ConvTree, maxPoints int, filterTags map[string]bo
 		result = append(result, treeEvents(tree.ChildTopRight, maxPoints, filterTags, start, finish)...)
 		return result
 	}
+}
+
+func extractTags(post data.Post, filterTags map[string]bool) []string {
+	tags := []string{}
+	r, _ := regexp.Compile("#[a-zA-Z0-9а-яА-Я_]+")
+	hashtags := r.FindAllString(post.Caption, -1)
+	for idx := range hashtags {
+		hashtags[idx] = strings.ToLower(hashtags[idx])
+	}
+	for _, tag := range hashtags {
+		tag := strings.ToLower(tag)
+		if filterTag(tag, filterTags) {
+			continue
+		}
+		tags = append(tags, tag)
+	}
+	r, _ = regexp.Compile("@[a-zA-Z0-9а-яА-Я_]+")
+	usernames := r.FindAllString(post.Caption, -1)
+	for idx := range usernames {
+		usernames[idx] = strings.ToLower(usernames[idx])
+	}
+	for _, tag := range usernames {
+		if filterTag(tag, filterTags) {
+			continue
+		}
+		tags = append(tags, tag)
+	}
+	return tags
+}
+
+func eventHolders(d []convtree.Point, filterTags map[string]bool) ([]eventHolder, []data.Post) {
+	evHolders := []eventHolder{}
+	posts := make([]data.Post, len(d))
+	for pi, p := range d {
+		post := p.Content.(data.Post)
+		posts[pi] = post
+		tags := extractTags(post, filterTags)
+		if len(tags) == 0 {
+			continue
+		}
+		h := eventHolder{
+			users: map[string]bool{},
+			posts: map[string]bool{},
+			tags:  map[string]int{},
+		}
+		h.users[post.AuthorID] = true
+		h.posts[post.Shortcode] = true
+		for _, tag := range tags {
+			h.tags[tag] = 1
+		}
+		found := false
+		for i := range evHolders {
+			if _, ok := evHolders[i].users[post.AuthorID]; ok {
+				h = combineHolders(evHolders[i], h)
+				evHolders[i] = h
+				found = true
+			}
+		}
+		if !found {
+			evHolders = append(evHolders, h)
+		}
+	}
+	res := []eventHolder{}
+	for i := 0; i < len(evHolders); i++ {
+		eh1 := evHolders[i]
+		for j := i + 1; j < len(evHolders); j++ {
+			eh2 := evHolders[j]
+			found := false
+			for t1 := range eh1.tags {
+				for t2 := range eh2.tags {
+					if t1 == t2 {
+						eh1 = combineHolders(eh1, eh2)
+						found = true
+						break
+					}
+				}
+				if found {
+					break
+				}
+			}
+			if found {
+				evHolders = append(evHolders[:j], evHolders[j+1:]...)
+				j--
+			}
+		}
+		res = append(res, eh1)
+	}
+	return res, posts
+}
+
+func combineHolders(eh1, eh2 eventHolder) eventHolder {
+	res := eventHolder{
+		users: map[string]bool{},
+		posts: map[string]bool{},
+		tags:  map[string]int{},
+	}
+	for u := range eh1.users {
+		res.users[u] = true
+	}
+	for u := range eh2.users {
+		res.users[u] = true
+	}
+	for p := range eh1.posts {
+		res.posts[p] = true
+	}
+	for p := range eh2.posts {
+		res.posts[p] = true
+	}
+	for t, c := range eh1.tags {
+		if _, ok := res.tags[t]; ok {
+			res.tags[t] += c
+		} else {
+			res.tags[t] = c
+		}
+	}
+	for t, c := range eh2.tags {
+		if _, ok := res.tags[t]; ok {
+			res.tags[t] += c
+		} else {
+			res.tags[t] = c
+		}
+	}
+	return res
+}
+
+func checkEvent(e eventHolder, maxPoints int, posts []data.Post, start, finish int64) (data.Event, bool) {
+	if len(e.users) < 2 {
+		return data.Event{}, false
+	}
+	if len(e.users) < maxPoints/2 {
+		return data.Event{}, false
+	}
+	for k, v := range e.tags {
+		if v < len(e.posts)/3 {
+			delete(e.tags, k)
+		}
+	}
+	postCodes := []string{}
+	for k := range e.posts {
+		postCodes = append(postCodes, k)
+	}
+	tags := []string{}
+	var max int
+	var maxTag string
+	for k := range e.tags {
+		if e.tags[k] > max {
+			max = e.tags[k]
+			maxTag = k
+		}
+		tags = append(tags, k)
+	}
+	return data.Event{
+		Center:    eventCenter(e.posts, posts),
+		PostCodes: postCodes,
+		Tags:      tags,
+		Title:     maxTag,
+		Start:     start,
+		Finish:    finish,
+	}, true
 }
 
 func eventCenter(codes map[string]bool, posts []data.Post) data.Point {
