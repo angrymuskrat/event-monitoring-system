cb0be7a902852f8f7fff61016cd4618e9b657b7e
visheratin <alexvish91@gmail.com>
2020-03-30 11:27:43 +0300

    Changed exposed headers.

--- services/backend/service/main.go
+++ services/backend/service/main.go
@@ -56,7 +56,7 @@ func accessControl(h http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		w.Header().Set("Access-Control-Expose-Headers", "Access-Token, Uid")
+		w.Header().Set("Access-Control-Expose-Headers", "Access-Token, Uid, Authorization, Set-Cookie")
 		if r.Method == "OPTIONS" {
 			return
 		}
