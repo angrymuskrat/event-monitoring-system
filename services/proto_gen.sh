8e46cb677d103b11ee681fdcc5610d961b1d6fd3
Mikhail Kovalchuk <mlhakov2011@gmail.com>
2021-11-08 14:20:47 +0300

    add fixing generated pg.go file generated by proto for event-detection service

--- services/proto_gen.sh
+++ services/proto_gen.sh
@@ -4,3 +4,4 @@ protoc --gofast_out=plugins=grpc:. data-storage/proto/data-storage.proto
 
 # service do not compile due to data-storage/proto/data-storage.pb.go cannot see "data", but with absolute go module path it worksbash
 perl -pi -e 's/proto1 "proto"/proto1 "github.com\/angrymuskrat\/event-monitoring-system\/services\/proto"/g' data-storage/proto/data-storage.pb.go
+perl -pi -e 's/proto1 "proto"/proto1 "github.com\/angrymuskrat\/event-monitoring-system\/services\/proto"/g' event-detection/proto/service.pb.go
